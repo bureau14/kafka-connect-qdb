86e73a1966f04826326823178514953869a11a92
terngkub <terngnattapol@gmail.com>
2020-04-10 15:15:32 +0200

    Bump version to 3.9.0.0-nightly.0.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.8.0-SNAPSHOT"
+QDB_API_VERSION="3.9.0-SNAPSHOT"
 
 echo "Cleaning java"
 rm -rf java \
