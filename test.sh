f2a2653d3493b94d2014014154449afcf264d165
Leon Mergen <leon@solatis.com>
2021-12-30 16:52:41 +0100

    Bump version to 3.10.3-nightly.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.10.2-SNAPSHOT"
+QDB_API_VERSION="3.10.3-SNAPSHOT"
 
 echo "Cleaning java"
 rm -rf java \
