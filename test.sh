0e2204e64fdbc470ca513a787aaac301b903ef92
terngkub <terngnattapol@gmail.com>
2023-08-02 14:43:09 +0700

    Bump version to 3.14.1-nightly.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ##
 # This is a helper script for development, do not use for CI or anything
 ##
-QDB_API_VERSION="3.14.0-SNAPSHOT"
+QDB_API_VERSION="3.14.1-SNAPSHOT"
 
 echo "Rebuilding JNI..."
 rm -rf jni \
